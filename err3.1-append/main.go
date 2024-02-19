8a956147e665ba71bc3985585b9bd6ec52e93285
shichuanshuan <shidashuan@qq.com>
2024-02-19 16:24:17 +0800

    praction err finish

--- err3.1-append/main.go
+++ err3.1-append/main.go
@@ -4,12 +4,24 @@ import "fmt"
 
 func main1() {
 	s := make([]int, 5)
-	s = append(s, 1, 2, 3)
+	fmt.Printf("%p\n", s)
+	s[0] = 0
+	s[1] = 1
+	fmt.Printf("%p\n", s)
 	fmt.Println(s)
 }
 
-func main2() {
+func main() {
 	s := make([]int, 0)
+	fmt.Printf("%p\n", s)
 	s = append(s, 1, 2, 3, 4)
+	fmt.Printf("%p\n", s)
 	fmt.Println(s)
+	fmt.Println("=============")
+
+	s1 := make([]int, 5)
+	fmt.Printf("%p\n", s1)
+	s1 = append(s1, 1, 2, 3, 4)
+	fmt.Printf("%p\n", s1)
+	fmt.Println(s1)
 }
--- err4.1-range/main.go
+++ err4.1-range/main.go
@@ -8,7 +8,7 @@ type Foo struct {
 	bar string
 }
 
-func main1() {
+func main() {
 	s1 := []Foo{
 		{"A"},
 		{"B"},
@@ -16,7 +16,7 @@ func main1() {
 	}
 	s2 := make([]*Foo, len(s1))
 	for i, value := range s1 {
-		fmt.Printf("i[%v] value[%v] valuePtr[%p]\n", i, value, &value)
+		fmt.Printf("i[%v] iPtr[%v] value[%v] valuePtr[%p]\n", i, &i, value, &value)
 		s2[i] = &value
 	}
 	fmt.Println(s1[0], s1[1], s1[2])
